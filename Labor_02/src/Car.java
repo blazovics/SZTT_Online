2dd5ac88ff1773028c5f62042419afc956733c3d
Laszlo Blazovics <blazovics.laszlo@aut.bme.hu>
2020-09-16 10:52:25 +0200

    override fix

--- Labor_02/src/Car.java
+++ Labor_02/src/Car.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Car implements IVehicle {
 	private ArrayList<Part> parts = new ArrayList<Part>();
+	@Override
 	public float calculateSpeed() {
 		return 130;
 	}
